d57c32539e54e27150236ae8a10b25115fa87f4a
ArEl3wam <54435155+ArEl3wam@users.noreply.github.com>
2021-08-25 07:11:09 +0200

    Create A - Team - codeforces.cpp
    http://codeforces.com/contest/231/problem/A

---
+++ A - Team - codeforces.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main() {
+   int n, a , b ,c;
+   cin >> n;
+   int cnt =0;
+   while(n--){
+       cin >> a >> b >> c;
+       if(a+b+c>=2){
+           cnt++;
+       }
+   }
+   cout << cnt ;
+    
+}
